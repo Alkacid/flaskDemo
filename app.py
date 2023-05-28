cf20a053004c94e1ab47f488ec8f82f927896624
ALKACID <alkacid@outlook.com>
2023-05-28 20:14:25 +0800

    添加较为完善的条件检索功能

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ usersql = {
 }
 
 cnx = mysql.connector.connect(**config)
+Description = None
 app = Flask(__name__)
 
 
@@ -49,59 +50,138 @@ def register():
 @app.route('/table')
 def index():
     cursor = cnx.cursor()
-
-    query = "describe book"
-    cursor.execute(query)
-    header = [col[0] for col in cursor.fetchall()]
-
-    query = "SELECT * FROM book"
+    cursor.execute("describe borrow")
+    Description = cursor.fetchall()
+    header = [col[0] for col in Description]
+    isInt = ['int' in str(col[1]) for col in Description]
+    isFloat = ['float' in str(col[1]) for col in Description]
+    isDate = ['date' in str(col[1]) for col in Description]
+    print(header)
+    colType = zip(isInt, isFloat, isDate)
+    query = "SELECT * FROM borrow"
     cursor.execute(query)
     data = cursor.fetchall()
     # print(data)
     # 确定表的长度和宽度
 
-    return render_template('table.html', header=header, data=data)
+    return render_template('table.html', header=header, data=data, colType=colType, zip=zip)
 
 
 @app.route('/table/sort', methods=['POST'])
 def sort_by():
+    print('you are here')
     if request.method == 'POST':
         # cnx = mysql.connector.connect(**config)
         cursor = cnx.cursor()
-        sortby = request.form['sortby']
-        direct = request.form['direct']
-        cursor.execute("SELECT * FROM book ORDER BY " + sortby + " " + direct)
+        sortItem = request.form['sortItem']
+        sortDirect = request.form['sortDirect']
+        cursor.execute("SELECT * FROM borrow ORDER BY " + sortItem + " " + sortDirect)
         data = cursor.fetchall()
-        query = "describe book"
+        query = "describe borrow"
         cursor.execute(query)
         header = [col[0] for col in cursor.fetchall()]
-        print(data)
-        print(header)
         return render_template('table.html', data=data, header=header)
 
 
-from flask import jsonify
+def create_html_table(header, data):
+    # Header row
+    header_row = "<tr>"
+    for column in header:
+        header_row += f"<th>{column}</th>"
+    header_row += "</tr>"
+
+    # Data rows
+    data_rows = ""
+    for item in data:
+        data_row = "<tr>"
+        for column in item:
+            data_row += f"<td>{column}</td>"
+        data_row += "</tr>"
+        data_rows += data_row
+
+    # Assemble the HTML table
+    html_table = f"""
+        <table class="table table-striped table-bordered table-hover"
+                       style="width: 80%; table-layout: auto; margin: auto auto" id="table">
+            <thead>
+                {header_row}
+            </thead>
+            <tbody>
+                {data_rows}
+            </tbody>
+        </table>
+    """
+
+    return html_table
 
 
-# 新路由/get-table-data，用于返回JSON格式的表格数据
-@app.route('/get-table-data')
+def create_mysql_query(formData):
+    sortItem = formData['sortItem']
+    sortDirect = formData['sortDirect']
+    del formData['sortItem']
+    del formData['sortDirect']
+    query = 'SELECT * FROM borrow  WHERE 1 = 1 '
+    for key in formData.keys():
+        if 'Lower' in key:
+            lowerbound = formData[key]
+        elif 'Upper' in key:
+            upperbound = formData[key]
+            if lowerbound == '' and upperbound == '':
+                continue
+            elif lowerbound != '' and upperbound != '':
+                column = key.replace('Upper', '')
+                query = query + " and %s between '%s' and '%s' " % (column, lowerbound, upperbound)
+            else:
+                column = key.replace('Upper', '')
+                if lowerbound != '':
+                    query = query + " and %s = '%s' " % (column, lowerbound)
+                if upperbound != '':
+                    query = query + " and %s = '%s' " % (column, upperbound)
+        else:
+            if formData[key] == '':
+                continue
+            query = query + " and %s = '%s' " % (key, formData[key])
+
+    if sortItem != 'default':
+        query = query + ' ORDER BY ' + sortItem + " " + sortDirect
+
+    return query
+
+
+@app.route('/get-table-data', methods=['POST'])
 def get_table_data():
+    formData = dict(request.form)
+    print(formData)
     cursor = cnx.cursor()
-    sortby = request.args.get('sortby', '')
-    direct = request.args.get('direct', '')  # 从请求获取搜索值参数
-    if sortby == 'default':
-        print('deeeeeefault')
-        cursor.execute("SELECT * FROM book")
-    else:
-        cursor.execute("SELECT * FROM book ORDER BY " + sortby + " " + direct)
-    rows = cursor.fetchall()
-    print(jsonify(rows).data)
-    # Return table data as JSON
-    return jsonify(rows)
+    # sortItem = formData['sortItem']
+    # sortDirect = formData['sortDirect']
+    # del formData['sortItem']
+    # del formData['sortDirect']
+    #
+    # query = 'SELECT * FROM borrow  WHERE 1 = 1 '
+    #
+    # for key in formData.keys():
+    #     if formData[key] == '':
+    #         continue
+    #     query = query + " and %s = '%s'" % (key, formData[key])
+    #
+    # if sortItem != 'default':
+    #     query = query + ' ORDER BY ' + sortItem + " " + sortDirect
+    query = create_mysql_query(formData)
+    print(query)
+    cursor.execute(query)
+    data = cursor.fetchall()
+    # print(data)
+    header = [col[0] for col in cursor.description]
+    # print(header)
+    table = create_html_table(header, data)
+    # print(table)
+    return table
 
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
+def login():
+    return redirect('/table')
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
